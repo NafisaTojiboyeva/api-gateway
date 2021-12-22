fa3963ee9c1953e3702d6a9d5341e539cdde9729
NafisaTojiboyeva <nafisa.tojiboyeva1999@gmail.com>
2021-12-22 14:26:15 +0500

    Initial upload

---
+++ config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"os"
+
+	"github.com/spf13/cast"
+)
+
+type Config struct {
+	Environment string // develop, staging, production
+
+	ToDoServiceHost string
+	ToDoServicePort int
+
+	// context timeout in seconds
+	CtxTimeout int
+
+	LogLevel string
+	HTTPPort string
+}
+
+func Load() Config {
+	c := Config{}
+
+	c.Environment = cast.ToString(getOrReturnDefault("ENVIRONMENT", "develop"))
+
+	c.LogLevel = cast.ToString(getOrReturnDefault("LOG_LEVEL", "debug"))
+	c.HTTPPort = cast.ToString(getOrReturnDefault("HTTP_PORT", ":8080"))
+	c.ToDoServiceHost = cast.ToString(getOrReturnDefault("TODO_SERVICE_HOST", "localhost"))
+	c.ToDoServicePort = cast.ToInt(getOrReturnDefault("TODO_SERVICE_PORT", 9000))
+
+	c.CtxTimeout = cast.ToInt(getOrReturnDefault("CTX_TIMEOUT", 7))
+
+	return c
+}
+
+func getOrReturnDefault(key string, defaultValue interface{}) interface{} {
+	_, exists := os.LookupEnv(key)
+	if exists {
+		return os.Getenv(key)
+	}
+
+	return defaultValue
+}
---
+++ service/service.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/resolver"
+
+	"github.com/NafisaTojiboyeva/api-gateway/config"
+	pb "github.com/NafisaTojiboyeva/api-gateway/genproto"
+)
+
+type IServiceManager interface {
+	ToDoService() pb.ToDoServiceClient
+}
+
+type serviceManager struct {
+	todoService pb.ToDoServiceClient
+}
+
+func (s *serviceManager) ToDoService() pb.ToDoServiceClient {
+	return s.todoService
+}
+
+func NewServiceManager(conf *config.Config) (serviceManager, error) {
+	resolver.SetDefaultScheme("dns")
+
+	connToDo, err := grpc.Dial(
+		fmt.Sprintf("%s:%d", conf.ToDoServiceHost, conf.ToDoServicePort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	serviceManager := &serviceManager{
+		todoService: pb.NewServiceClient(connToDo),
+	}
+
+	return serviceManager, nil
+}
